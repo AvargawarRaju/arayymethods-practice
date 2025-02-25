ca319f6dfe51324d9d6f3c652065d8c478d46a5c
AwargawarRaju <aurgyaraju@gmail.com>
2025-02-25 15:41:17 +0530

    added

---
+++ index.js.js
@@ -0,0 +1,86 @@
+// let n=4
+// for(let i=1;i<=n;i++){
+//     let line=""
+//     for(k=1;k<=n-i;k++){
+//         line+=" "
+//     }
+//     for(let j=1;j<=i;j++){
+//         line=j
+//     }
+//     for(let j=i-1;j>1;j--){
+//         line+=j
+//     }console.log(line)
+// }
+
+
+
+
+// 1
+// 4 7
+// 9 12 14
+// 15 18 20 21
+// r=4
+// count=1
+// for(i=1;i<=r;i++){                             
+//     s="" 
+   
+     
+//      for(j=1;j<=i;j++){
+//         s=s+count+" "
+//         count=count+(r+j)
+//      }
+    
+// } console.log(s)
+
+
+
+//array methods
+
+// let arr = [10, 20, 30, 40];
+// console.log(arr.indexOf(55)); // 2
+
+
+// let arr = ['apple', 'banana', 'cherry'];
+// console.log(arr.includes('banana')); // true
+
+// let arr = [5, 10, 15, 20];
+// let result = arr.find(num => num > 10);
+// console.log(result); // 15
+
+
+// let arr = [5, 10, 15, 20];
+// console.log(arr.findIndex(num => num > 10)); // 2
+
+// let arr = [1, 2, 3];
+// let squared = arr.map(num => num * num);
+// console.log(squared); // [1, 4, 9]
+
+// let arr = [1, 2, 3, 4];
+// let sum = arr.reduce((a, num) => a + num, );
+// console.log(sum); // 10
+
+// let arr = [10, 20, 30];
+// arr.forEach(num => console.log(num * 2));
+// 20
+// 40
+// 60
+// let arr = [30, 10, 50, 20,15];
+// arr.sort((a, b) => a - b); // Ascending order
+// console.log(arr); // [10, 20, 30, 50]
+
+// let arr = [1, 2, 3];
+// arr.reverse();
+// console.log(arr);
+//  // [3, 2, 1]
+
+// let arr = [10, 20, 30, 40, 50];
+// console.log(arr.slice(2, 4)); 
+// // [20, 30]
+
+// let arr = [1, 2, 6, 7, 8];
+// let arr1=arr.splice(2,1);
+// console.log(arr1); 
+  
+// let arr = [10, 20, 10];
+// console.log(arr.every(num => num > 5)); // true
+
